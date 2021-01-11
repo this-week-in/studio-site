600bf380b8f1a4c8675023f5b0988961fa725f5c
Josh Long <josh@joshlong.com>
2021-01-10 18:15:30 -0800

    deploy

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
+TAG_NAME=${1:-$(date +%s)}
 ROOT=$( cd $(dirname $0)  && pwd )
+
 cd $ROOT
+
 docker build -t twi-editor-view .
+git tag $TAG_NAME
+git push origin $TAG_NAME
+
