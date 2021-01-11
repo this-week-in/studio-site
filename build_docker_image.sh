d9922b9c5dd4615cce74b6a4a313b7b303797d35
Josh Long <josh@joshlong.com>
2021-01-10 18:56:47 -0800

    deploy

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 set -o pipefail
-
+##
+##
+APP_NAME=twi-editor-view
+PROJECT_ID=${GCLOUD_PROJECT}
 TAG_NAME=${1:-$(date +%s)}
-ROOT=$( cd $(dirname $0)  && pwd )
-
-cd $ROOT
-
-docker build -t twi-editor-view .
-git tag $TAG_NAME
-git push origin $TAG_NAME
+IMAGE_TAG="production${GITHUB_SHA:-}"
+GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
+IMAGE_NAME=${GCR_IMAGE_NAME}:${IMAGE_TAG}
+##
+##
+cd $( cd $(dirname $0)  && pwd )
+docker build -t $APP_NAME .
+image_id=$(docker images -q $APP_NAME)
+docker tag "${image_id}" $IMAGE_NAME
+docker push $IMAGE_NAME
+echo "pushing ${image_id} to $IMAGE_NAME "
+echo "tagging ${GCR_IMAGE_NAME}"
 
